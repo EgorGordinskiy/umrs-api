0fd0d475784f397e628cf09d64632d9fc67a51e4
Eofre <yegorgordinskiy2002@gmail.com>
2025-02-16 00:22:18 +0800

    chore: setup migrations

---
+++ src/typeorm.config.ts
@@ -0,0 +1,26 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+export default AppDataSource;
