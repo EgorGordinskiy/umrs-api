ebb3f101627d3e46a34f255c3ef3c1c976844b39
volard <kirillzhilenkov@gmail.com>
2025-04-19 18:12:40 +0800

    feat(module/survey): добавить entities к TypeORM

--- src/modules/survey/survey.module.ts
+++ src/modules/survey/survey.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyResponseModule } from './survey-response/survey-response.module';
 import { SurveySchemaModule } from './survey-schema/survey-schema.module';
+import { Survey } from './survey.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [SurveyService],
-  imports: [SurveyResponseModule, SurveySchemaModule],
+  imports: [
+    SurveyResponseModule,
+    SurveySchemaModule,
+    TypeOrmModule.forFeature([Survey]),
+  ],
 })
 export class SurveyModule {}
