45f1af01dc86ad232771f4a0601857d0913fc265
volard <kirillzhilenkov@gmail.com>
2025-04-20 23:50:41 +0800

    feat(module/auth): требовать в guard authorization токен

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -1,13 +1,46 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { OpenIdService } from './openid.service';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
+
+  constructor(private openIdService: OpenIdService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    console.log(request);
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    this.logger.log('Token: ' + token);
+
+    // const tokenInfo = await this.openIdService.validateToken(token);
+    //
+    // if (!tokenInfo || !tokenInfo.active) {
+    //   throw new UnauthorizedException();
+    // }
+
+    // Attach the token information to the request for use in controllers
+    // request['user'] = tokenInfo;
+
     return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
