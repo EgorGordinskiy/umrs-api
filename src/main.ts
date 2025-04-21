f151cb24f5efd8f13b1e3e5fba70256b0ae43d39
volard <kirillzhilenkov@gmail.com>
2025-04-22 06:43:38 +0800

    feat: добавить HTTPS
    Иначе OAuth не даст нормально работать.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/usr/src/app/key.pem'),
+    cert: fs.readFileSync('/usr/src/app/cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.setGlobalPrefix('api');
 
