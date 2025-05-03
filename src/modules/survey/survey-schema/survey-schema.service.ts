a45348543809cc2d7af78a2bbf5490a32dee8831
volard <kirillzhilenkov@gmail.com>
2025-05-03 16:53:34 +0800

    refactor(survey schema): убрать контроллеры и связанную логику
    Схемы, как я понимаю, не существуют сами по себе. Во всяком
    случае пока в этом нужды нет. Поэтому они (а потом и их версии) будут
    управляться из модуля Survey при операциях над связанной со схемой анкетой.
    Помимо организации расположения логики работы с сущностями, решается
    проблема цикличной зависимости (Survey <-> SurveySchema).

--- src/modules/survey/survey-schema/survey-schema.service.ts
+++ src/modules/survey/survey-schema/survey-schema.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SurveySchema } from './survey-schema.entity';
 import { BaseServiceImpl } from '../../../common/abstract';
-import { UpdateSurveySchemaDto } from './dto/update-survey-schema.dto';
-import { CreateSurveySchemaDto } from './dto/create-survey-schema.dto';
+import { UpdateSurveySchemaDto } from '../dto/update-survey-schema.dto';
+import { CreateSurveySchemaDto } from '../dto/create-survey-schema.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Survey } from '../survey.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -16,8 +15,6 @@ export class SurveySchemaService extends BaseServiceImpl<
   constructor(
     @InjectRepository(SurveySchema)
     repository: Repository<SurveySchema>,
-    @InjectRepository(Survey)
-    private surveyRepository: Repository<Survey>,
   ) {
     super(repository);
   }
@@ -26,31 +23,31 @@ export class SurveySchemaService extends BaseServiceImpl<
     return this.repository.save(this.make(dto));
   }
 
-  public async getBySurveyId(surveyId: string): Promise<SurveySchema> {
-    const found = await this.repository.findOne({
-      relations: { surveys: true },
-      where: { surveys: { id: surveyId } },
-    });
-    if (!found) {
-      throw new NotFoundException(
-        `Сущность SurveySchema через Survey с ID ${surveyId} не найдена.`,
-      );
-    }
-    return found;
-  }
-
-  public async getByResponseId(responseId: string): Promise<SurveySchema> {
-    const found = await this.repository.findOne({
-      relations: { surveys: { responses: true } },
-      where: { surveys: { responses: { id: responseId } } },
-    });
-    if (!found) {
-      throw new NotFoundException(
-        `Сущность SurveySchema через SurveyResponse с ID ${responseId} не найдена.`,
-      );
-    }
-    return found;
-  }
+  // public async getBySurveyId(surveyId: string): Promise<SurveySchema> {
+  //   const found = await this.repository.findOne({
+  //     relations: { surveys: true },
+  //     where: { surveys: { id: surveyId } },
+  //   });
+  //   if (!found) {
+  //     throw new NotFoundException(
+  //       `Сущность SurveySchema через Survey с ID ${surveyId} не найдена.`,
+  //     );
+  //   }
+  //   return found;
+  // }
+  //
+  // public async getByResponseId(responseId: string): Promise<SurveySchema> {
+  //   const found = await this.repository.findOne({
+  //     relations: { surveys: { responses: true } },
+  //     where: { surveys: { responses: { id: responseId } } },
+  //   });
+  //   if (!found) {
+  //     throw new NotFoundException(
+  //       `Сущность SurveySchema через SurveyResponse с ID ${responseId} не найдена.`,
+  //     );
+  //   }
+  //   return found;
+  // }
 
   update(id: string, dto: UpdateSurveySchemaDto): Promise<SurveySchema> {
     return this.repository.save(this.make(dto));
@@ -64,10 +61,17 @@ export class SurveySchemaService extends BaseServiceImpl<
     });
   }
 
+  public async hasLinkedSurveys(id: string): Promise<boolean> {
+    return await this.repository
+      .createQueryBuilder('schema')
+      .leftJoin('schema.surveys', 'survey')
+      .where('schema.id = :id', { id })
+      .andWhere('survey.id IS NOT NULL')
+      .getExists();
+  }
+
   public async delete(id: string): Promise<boolean> {
-    const linkedSurveysExist = await this.surveyRepository.existsBy({
-      schema: { id },
-    });
+    const linkedSurveysExist = await this.hasLinkedSurveys(id);
     if (linkedSurveysExist) {
       throw new Error(
         `Нельзя удалить схему с ID ${id}, которая используется в анкетах`,
