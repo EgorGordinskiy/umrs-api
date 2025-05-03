40ece325d6f656df2c2c1e9e3b8f2e9ac06f27c6
volard <kirillzhilenkov@gmail.com>
2025-05-03 16:28:14 +0800

    feat: возвращать полные сообщения об ошибке в dev окружении

---
+++ src/common/debug-exceptions.filter.ts
@@ -0,0 +1,99 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+/**
+ * Определяет структуру ответов на ошибки HTTP.
+ */
+interface ErrorResponse {
+  /** Код состояния HTTP. */
+  statusCode: number;
+  /** Метка времени ответа. */
+  timestamp: string;
+  /** Сообщение об ошибке. */
+  message: string;
+  /** Путь запроса. */
+  path?: string;
+  /** Трассировка стека. */
+  stack?: string;
+  /** Имя ошибки. */
+  name?: string;
+}
+
+@Catch()
+export class DebugExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+  private readonly skipExceptions = [UnauthorizedException];
+
+  /**
+   * Обрабатывает исключения, пойманные во время обработки HTTP-запроса, пропуская указанные типы исключений.
+   * Формирует исключение в структурированный ответ об ошибке.
+   *
+   * @param exception - Исключение, брошенное во время обработки запроса.
+   * @param host - Текущий хост аргументов, содержащий контекст запроса.
+   */
+  catch(exception: unknown, host: ArgumentsHost): void {
+    // не обрабатывать исключения, для которых, например, есть другой фильтр
+    if (
+      this.skipExceptions.some((_exception) => exception instanceof _exception)
+    ) {
+      return;
+    }
+
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = DebugExceptionsFilter.createErrorResponse(
+      exception,
+      httpStatus,
+    );
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+
+  /**
+   * Создает структурированный объект ответа на ошибку на основе предоставленного исключения и кода состояния.
+   * Обрабатывает HttpException, общие Error и другие типы, включая трассировку стека и имя, если оно доступно.
+   *
+   * @param exception Исключение для форматирования.
+   * @param statusCode Код состояния HTTP для включения в ответ.
+   * @returns Объект `ErrorResponse`, содержащий сведения об ошибке.
+   */
+  private static createErrorResponse(
+    exception: unknown,
+    statusCode: number,
+  ): ErrorResponse {
+    const base: ErrorResponse = {
+      statusCode,
+      timestamp: new Date().toISOString(),
+      message: '',
+    };
+
+    if (exception instanceof HttpException) {
+      const exceptionResponse = exception.getResponse();
+      base.message =
+        typeof exceptionResponse === 'string'
+          ? exceptionResponse
+          : JSON.stringify(exceptionResponse);
+    } else if (exception instanceof Error) {
+      base.message = exception.message;
+      base.stack = exception.stack;
+      base.name = exception.name;
+    } else {
+      base.message = String(exception);
+    }
+
+    return base;
+  }
+}
