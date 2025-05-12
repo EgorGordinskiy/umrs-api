aedc03dcbe95dcf35746bfe662a429198d243db4
volard <kirillzhilenkov@gmail.com>
2025-05-13 01:04:35 +0800

    refactor(cli/runSeeder): правильно выводить ошибку

--- src/database/cli/runSeeder.ts
+++ src/database/cli/runSeeder.ts
@@ -1,6 +1,6 @@
 import { seedDatabase } from '../functions';
 import { Logger } from '@nestjs/common';
 
-seedDatabase().catch((err) => {
-  Logger.error('Error during seeding:', err);
+seedDatabase().catch((err: Error) => {
+  Logger.error(err.message, err.stack, err.name);
 });
